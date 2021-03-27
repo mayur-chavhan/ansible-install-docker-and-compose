3c423c95b590e8f4a0b853dd6aff8d95d13e0ee1
mayur-chavhan <b.chavhan.b@gmail.com>
2021-03-27 23:22:56 +0530

    Added docker gpg key and some dependecies

--- install-docker.sh
+++ install-docker.sh
@@ -23,6 +23,21 @@ elif _cmdfnd ansible; then
 
 		echo -e "\n\t[X] ==> Installing docker and docker-compose using ansible playbook \n"
 
+
+		
+ 		sudo apt-get install \
+		apt-transport-https \
+		ca-certificates \
+		curl \
+		gnupg \
+		lsb-release -qq -y > /dev/null < /dev/null
+
+		# Remove below lines if docker repo key is not added 
+		# echo -e "\n\t[X] ==> Adding Docker GPG key \n"
+		#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+		sudo apt-get update > /dev/null < /dev/null
+
 		sudo ansible-playbook main.yml
 
 else
